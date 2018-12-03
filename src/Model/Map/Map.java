5ddc28b76387cd190f2bc2aba51dbe60895157f5
Mohammad.Sj <mohammad.sj2@gmail.com>
2018-12-03 18:29:21 +0330

    +CellDoesNotExist Class

--- src/Model/Map/Map.java
+++ src/Model/Map/Map.java
@@ -3,6 +3,7 @@ package Model.Map;
 import Model.Entity.Animal.Wild.Wild;
 import Model.Entity.Entity;
 import Model.Entity.Item;
+import Exception.CellDoesNotExist;
 
 import java.util.ArrayList;
 
@@ -17,16 +18,16 @@ public class Map {
     }
 
 
-    private Cell getCell(int x, int y) {
+    private Cell getCell(int x, int y) throws CellDoesNotExist {
         for (Cell cell : cells) {
             if (cell.getPositionX() == x && cell.getPositionY() == y) {
                 return cell;
             }
         }
-        return null;
+        throw new CellDoesNotExist();
     }
 
-    public void plant(int x, int y) {
+    public void plant(int x, int y) throws CellDoesNotExist{
         Cell cell = getCell(x, y);
         cell.plantGrass();
     }
@@ -76,12 +77,12 @@ public class Map {
         return nearestCell(cell, cellsWithWild);
     }
 
-    public ArrayList<Item> getItems(int x, int y) {
+    public ArrayList<Item> getItems(int x, int y) throws CellDoesNotExist{
         Cell cell = getCell(x, y);
         return cell.getItems();
     }
 
-    public void destroyEntity(int x, int y, Entity entity) {
+    public void destroyEntity(int x, int y, Entity entity) throws CellDoesNotExist{
         Cell cell = getCell(x, y);
         cell.destroyEntity(entity);
     }
@@ -94,7 +95,7 @@ public class Map {
         return items;
     }
 
-    public ArrayList<Item> cage(int x, int y) {
+    public ArrayList<Item> cage(int x, int y) throws CellDoesNotExist{
         Cell cell = getCell(x, y);
         return wildsToItems(cell.getWilds());
     }
