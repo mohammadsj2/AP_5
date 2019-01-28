8f37947c8d1eed6ec86b5076f66194a7a6a8cf1e
Mohammad.Sj <mohammad.sj2@gmail.com>
2019-01-28 12:07:25 +0330

    +Network classes

--- src/Network/Server/Server.java
+++ src/Network/Server/Server.java
@@ -1,4 +1,38 @@
 package Network.Server;
 
+import Network.Address;
+import Network.Chatroom;
+import Network.Client.Client;
+import sun.misc.Cleaner;
+import Exception.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Server {
+    private ArrayList<Client> clients=new ArrayList<>();
+    private Address address;
+    private Chatroom globalChatroom;
+    private ArrayList<ArrayList<Chatroom>> privateChatrooms=new ArrayList<>();
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Chatroom getGlobalChatroom() {
+        return globalChatroom;
+    }
+    private int getClientId(Client client) throws ClientDoesNotExist {
+        for (int i = 0; i < clients.size(); i++) {
+            Client client1 = clients.get(i);
+            if (client1.equals(client)) {
+                return i;
+            }
+        }
+        throw new ClientDoesNotExist();
+    }
+    public Chatroom getPrivateChatroom(Client a,Client b) throws ClientDoesNotExist {
+        int i=getClientId(a);
+        int j=getClientId(b);
+        return privateChatrooms.get(i).get(j);
+    }
 }
