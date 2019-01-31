d7fe961b435d5c5928c960841e687ce3e70f3e8e
Sa1378 <mehrdadsaberi1378@gmail.com>
2019-02-01 02:39:07 +0330

    add Reply in Chatroom

--- src/Network/Message.java
+++ src/Network/Message.java
@@ -2,28 +2,66 @@ package Network;
 
 import Network.Client.Client;
 
-public class Message {
+public class Message
+{
     String text;
     Client client;
+    // Message replied;
+    String repliedText;
+    Client repliedClient;
 
-    public Message(Client client, String text) {
+    public Message(Client client, String text, Message replied)
+    {
         this.text = text;
         this.client = client;
+        if (replied != null)
+        {
+            this.repliedText = replied.getText();
+            this.repliedClient = replied.getClient();
+        } else
+        {
+            repliedText = null;
+            repliedClient = null;
+        }
     }
 
-    public String getText() {
+    public String getText()
+    {
         return text;
     }
 
-    public void setText(String text) {
+    public void setText(String text)
+    {
         this.text = text;
     }
 
-    public Client getClient() {
+    public Client getClient()
+    {
         return client;
     }
 
-    public void setClient(Client client) {
+    public void setClient(Client client)
+    {
         this.client = client;
+    }
+
+    public String getRepliedText()
+    {
+        return repliedText;
+    }
+
+    public void setRepliedText(String repliedText)
+    {
+        this.repliedText = repliedText;
+    }
+
+    public Client getRepliedClient()
+    {
+        return repliedClient;
+    }
+
+    public void setRepliedClient(Client repliedClient)
+    {
+        this.repliedClient = repliedClient;
     }
 }
